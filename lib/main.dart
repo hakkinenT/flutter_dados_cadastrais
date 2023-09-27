1089e8b5a4c5e9e8d457172cea61010f8d997704
Tawanna Hakkinen <thol_16@hotmail.com>
2023-09-26 21:08:23 -0300

    remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dados_cadastrais/db/db.dart';
 import 'package:flutter_dados_cadastrais/utils/constants.dart';
 import 'package:flutter_dados_cadastrais/utils/view/pages/home_page.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
-
-import 'utils/models/register_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
